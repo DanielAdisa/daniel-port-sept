ef7be4bba896615d4ce20fd867c2b7c11858a84e
Your Name <you@example.com>
2024-09-16 22:35:32 +0100

    finsihed no animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Contact from "./components/Contact"
+import Experience from "./components/Experience"
 import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
@@ -15,6 +17,8 @@ const App = () => {
           <Hero />
           <Technologies />
           <Projects />
+          <Experience />
+          <Contact />
       </div>
     </div>
   )
