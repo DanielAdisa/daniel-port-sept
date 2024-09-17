3b87b50cec3ef05265414108699fcbdee679923b
Your Name <you@example.com>
2024-09-17 06:32:45 +0100

    asas

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ const App = () => {
           <Hero />
           <Technologies />
           <Projects />
-          {/* <Experience /> */}
+          <Experience />
           <Contact />
       </div>
     </div>
