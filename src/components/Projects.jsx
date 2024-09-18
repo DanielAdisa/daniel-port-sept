7fc1215a579b5c272a8d1c19b438db9f08ed1330
Your Name <you@example.com>
2024-09-18 17:53:10 +0100

    xcxc

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -20,18 +20,25 @@ const Projects = () => {
                         <img src={project.image} alt={project.title} className="mb-6 rounded " width={250} height={250} />
                     </motion.div>
                     <motion.div 
-                        whileInView={{opacity: 1, x:0}}
-                        initial={{opacity:0 , x:100}}
-                        transition={{duration: 1.5}}
+                        // whileInView={{opacity: 1, x:0}}
+                        // initial={{opacity:0 , x:100}}
+                        // transition={{duration: 1.5}}
                         className="w-full max-w-full my-auto lg:w-3/4">
                             <h3 className="mb-2 text-2xl font-semibold">{project.title}</h3>
                             <p className="mb-4 text-left text-stone-400">{project.description}</p>
-                            {project.technologies.map((tech, index) => (
-                                <span className="p-2 mr-2 text-sm font-medium rounded bg-stone-900 text-stone-300" key={index}>
-                                    {tech}
-                                </span>
-                            ))}
+                            
                     </motion.div>
+                    <div className="flex flex-row clear-both w-full ">
+                            {project.technologies.map((tech, index) => (
+                                <motion.span
+                                    whileInView={{opacity: 1, y:0}}
+                                    initial={{opacity:0 , y:100}}
+                                    transition={{duration:2}}
+                                    className="p-2 mr-2 text-sm font-medium rounded bg-stone-900 text-stone-300" key={index}>
+                                        {tech}
+                                </motion.span>
+                            ))}
+                            </div>
                 </div>
             ))}
         </div>
