d357506012153bb5fc3270d2ebfcfebc10604269
Your Name <you@example.com>
2024-09-19 21:24:58 +0100

    navxc

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { FaGithub, FaInstagram, FaLinkedin, FaWhatsapp } from "react-icons/fa";
 import Asset from "../assets/Asset.webp";
+import { motion } from "framer-motion";
 
 const Navbar = () => {
   return (
@@ -11,21 +12,37 @@ const Navbar = () => {
         </div>
         <div className="flex items-center justify-center gap-4 m-8 text-2xl">
 
-          <a href="https://www.linkedin.com/in/daniel-oluwatobi-adisa-7b076a199/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+          <motion.a 
+           whileInView={{opacity: 1, y:0}}
+           initial={{opacity:0 , y:50}}
+           transition={{duration:0.5 }}
+            href="https://www.linkedin.com/in/daniel-oluwatobi-adisa-7b076a199/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
             <FaLinkedin/>
-          </a>
+          </motion.a>
 
-          <a href="https://github.com/DanielAdisa/" target="_blank" rel="noopener noreferrer" aria-label="Github">
+          <motion.a 
+          whileInView={{opacity: 1, y:0}}
+          initial={{opacity:0 , y:50}}
+          transition={{duration:1 }}
+          href="https://github.com/DanielAdisa/" target="_blank" rel="noopener noreferrer" aria-label="Github">
             <FaGithub/>
-          </a>
+          </motion.a>
 
-          <a href="https://www.instagram.com/daniel.made.it?igsh=MXY2Nmt3bWRvaHJxdQ==" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+          <motion.a 
+          whileInView={{opacity: 1, y:0}}
+          initial={{opacity:0 , y:50}}
+          transition={{duration:1.5 }}
+          href="https://www.instagram.com/daniel.made.it?igsh=MXY2Nmt3bWRvaHJxdQ==" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
             <FaInstagram/>
-          </a>
+          </motion.a>
 
-          <a href="https://wa.me/message/V4TC5GSQTN7RM1" target="_blank" rel="noopener noreferrer" aria-label="Whatsapp">
+          <motion.a 
+          whileInView={{opacity: 1, y:0}}
+          initial={{opacity:0 , y:50}}
+          transition={{duration:2 }}
+          href="https://wa.me/message/V4TC5GSQTN7RM1" target="_blank" rel="noopener noreferrer" aria-label="Whatsapp">
             <FaWhatsapp/>
-          </a>
+          </motion.a>
           
         </div>
     </nav>
